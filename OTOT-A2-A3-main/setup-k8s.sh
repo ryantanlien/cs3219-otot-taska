cfc26cec62c38f62122cb6dd90e42d393e5b7e30
Ryan Tan <ryantanlien99@gmail.com>
2022-10-10 02:05:12 +0800

    Add A3 yamls

--- OTOT-A2-A3-main/setup-k8s.sh
+++ OTOT-A2-A3-main/setup-k8s.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 kind create cluster --name kind-1 --config k8s/kind/cluster-config.yaml
-kubectl apply -f ./k8s/manifests/backend-deployment.yaml
+kubectl apply -f ./k8s/manifests/backend-deployment-zone-aware.yaml
 kubectl apply -f ./k8s/manifests/backend-service.yaml
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
 kubectl apply -f ./k8s/manifests/backend-ingress.yaml
